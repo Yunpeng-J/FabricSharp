77dfc46567d967c7e795aacaca287d6c3d612e1b
yunpeng <ypjiang@cs.hku.hk>
2021-01-18 18:06:14 +0800

    disable checking txid

--- core/endorser/msgvalidation.go
+++ core/endorser/msgvalidation.go
@@ -155,7 +155,7 @@ func (up *UnpackedProposal) Validate(idDeserializer msp.IdentityDeserializer) er
 	}
 
 	expectedTxID := protoutil.ComputeTxID(up.SignatureHeader.Nonce, up.SignatureHeader.Creator)
-	if up.TxID() != expectedTxID {
+	if (false && (up.TxID() != expectedTxID)) {
 		return errors.Errorf("incorrectly computed txid '%s' -- expected '%s'", up.TxID(), expectedTxID)
 	}
 
